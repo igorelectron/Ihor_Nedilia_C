d220e495a8e37df858a0ae8b4df9173f6851bd26
igorelectron <139206915+igorelectron@users.noreply.github.com>
2023-12-12 13:22:51 +0200

    Lab 14. Ihor Nedilia. Compmath-1. 2 course

---
+++ task4.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <vector>
+
+
+int main() {
+    freopen("data/lab14_task4.txt", "r", stdin);
+
+    std::string s;
+    std::vector<std::string> tag_stack;
+    while (std::cin >> s) {
+        if (s.starts_with('<')) {
+            if (!s.ends_with('>')) {
+                std::cout << "incorrect: missing `>`\n";
+                return 1;
+            }
+            if (s.starts_with("</")) {
+                if (tag_stack.empty()) {
+                    std::cout << "incorrect: no open tag\n";
+                    return 1;
+                }
+                if (tag_stack.back() != std::string_view(s).substr(2, s.size()-3)) {
+                    std::cout << "incorrect: tags doesn't match\n";
+                    return 1;
+                }
+                tag_stack.pop_back();
+            } else {
+                tag_stack.push_back(s.substr(1, s.size()-2));
+            }
+        }
+    }
+    std::cout << "correct\n";
+}
